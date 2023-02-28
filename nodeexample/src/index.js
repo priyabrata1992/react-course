194b9ae4d9575bd13d45300afc29c0a380b7c3d5
Priyabrata Chakraverti <pchakraverti@adobe.com>
2023-02-28 11:28:18 +0530

    higher order functions.

--- nodeexample/src/index.js
+++ nodeexample/src/index.js
@@ -1,4 +1,9 @@
-console.log('works');
-setInterval(()=>{
-    console.log('print every 1000 ms');
-}, 1000);
+let { forEach, filter } = require('./lib');
+
+let data = [1, 4, 2, 5, 7, 9, 3];
+// forEach(data, console.log);
+
+isOdd = (e) => e % 2 !== 0;
+let r1 = filter(data, function(e) { return e % 2 === 0 });
+let r2 = filter(data, isOdd);
+forEach(r2, console.log);
---
+++ nodeexample/src/lib.js
@@ -0,0 +1,43 @@
+//Functional style of programming
+//Higher order functions:
+/*
+    A function that -
+        1. Accept a function as a param.
+        2. Return a function.
+*/  
+
+//Pure function: forEach
+function forEach(elems) {
+    for(i = 0; i < elems.length; i++) {
+        console.log(elems[i]);
+    }
+}
+
+// Higher order equivalent: forEach
+function forEach(elems, action) {
+    for(i = 0; i < elems.length; i++) {
+        action(elems[i]);
+    }
+}
+
+//HOF examples: filter, map, reduce
+// Filter --> return a subset based on a predicate function
+function filter(elements, predicate) {
+    let result = [];
+    forEach(elements, function(e){
+        if (predicate(e)) {
+            result.push(e);
+        }
+    });
+    return result;
+}
+
+/* 
+    Everything is private by default
+    in order to make it public, it needs to be exported.
+*/
+
+module.exports = {
+    forEach,
+    filter
+}
